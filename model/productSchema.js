61afde28d62729fcc6a10ff24a7aa035536af137
lalit-shelkar <lalitshelkar2424@gmail.com>
2024-07-03 15:14:28 +0530

    .

--- model/productSchema.js
+++ model/productSchema.js
@@ -12,6 +12,10 @@ const productSchema = new mongoose.Schema(
             type:Number,
             required:true,
         },
+        price:{
+            type:Number,
+            required:true,
+        },
         img:{
             type:String,
             required:true,
